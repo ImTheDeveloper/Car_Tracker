9021af0ef00920b2ae802de05c7247611a63fc01
christoface <chrismobberley@gmail.com>
2013-09-19 21:25:16 +0100

    Loop counter
    Addition of a loop counter to knock the slave arduino back out if there
    is no data received from the master

--- Car_Tracker_Slave/Car_Tracker_Slave.ino
+++ Car_Tracker_Slave/Car_Tracker_Slave.ino
@@ -25,15 +25,15 @@ SoftEasyTransfer ET;
 struct RECEIVE_DATA_STRUCTURE{
   float latitude;
   float longitude;
- // char lon_string;
- // char lat_string;
+  // char lon_string;
+  // char lat_string;
 };
 
 //Declarations
 RECEIVE_DATA_STRUCTURE mydata;
 char latty[20];
 char longy[20];
-
+int loop_counter=0;
 // ***************************************
 // Setup
 // ***************************************
@@ -41,9 +41,11 @@ char longy[20];
 void setup(){
   pinMode(2,INPUT);
   digitalWrite(2,HIGH);
+
   Serial.begin(38400); //Debug Serial
   mySerial2ndDuno.begin(9600); //Communication Serial
   ET.begin(details(mydata), &mySerial2ndDuno);  //Expect communication using data structure
+
   Serial.println("Initialisation complete.");
   Serial.println("Going to sleep"); 
   delay(100); //Ensure serial print has time to run  
@@ -56,25 +58,33 @@ void setup(){
 
 
 void loop(){
-  
-//Check and see if a data packet has come in.
+
+  //Check and see if a data packet has come in.
   if(ET.receiveData()){  
-       Serial.println(mydata.latitude);
-       Serial.println(mydata.longitude);     
-//Convert the inbound floats/doubles/ints into string ready for sending to json
-       String lat = dtostrf(convertDegMinToDecDeg(mydata.latitude),10,6,latty); 
-       String lon = dtostrf(convertDegMinToDecDeg(mydata.longitude),10,6,longy);
-//Trim and turn the inbound data in to correct negative or positives // Need to pass in GPS.lon and GPS.lat at later date
-      lat.trim();
-      lon.trim();
-//Setup required JSON for GSM dispatch - DEBUG PRINT ATM
-      Serial.print("{\"location\": {\"disposition\": \"mobile\",\"name\": \"Car Location\",\"exposure\": \"outdoor\", \"domain\": \"physical\",\"ele\": \"0000\",\"lat\": "+lat+",\"lon\": "+lon+"}}");
-//Final step is to sleep the arduino and await for our next interrupt.
-      Knock_Out();
-    }
-//Addition of a delay to ensure transmission can be caught.
-Serial.println("Looping");
+    Serial.println(mydata.latitude);
+    Serial.println(mydata.longitude);     
+    //Convert the inbound floats/doubles/ints into string ready for sending to json
+    String lat = dtostrf(convertDegMinToDecDeg(mydata.latitude),10,6,latty); 
+    String lon = dtostrf(convertDegMinToDecDeg(mydata.longitude),10,6,longy);
+    //Trim and turn the inbound data in to correct negative or positives // Need to pass in GPS.lon and GPS.lat at later date
+    lat.trim();
+    lon.trim();
+    //Setup required JSON for GSM dispatch - DEBUG PRINT ATM
+    Serial.print("{\"location\": {\"disposition\": \"mobile\",\"name\": \"Car Location\",\"exposure\": \"outdoor\", \"domain\": \"physical\",\"ele\": \"0000\",\"lat\": "+lat+",\"lon\": "+lon+"}}");
+    //Final step is to sleep the arduino and await for our next interrupt.
+    Knock_Out();
+  }
+  //Addition of a delay to ensure transmission can be caught.
+  Serial.println("Looping");
+  
   delay(250);
+  
+  loop_counter++
+    if (loop_counter>10)
+  {
+    loop_counter=0;
+    Knock_Out();   
+  }
 }
 
 
@@ -104,7 +114,7 @@ Serial.println("Looping");
 //  return s;
 //}
 
-  // degree-minute format to decimal-degrees
+// degree-minute format to decimal-degrees
 double convertDegMinToDecDeg (float degMin) {
   double min = 0.0;
   double decDeg = 0.0;
@@ -119,20 +129,20 @@ double convertDegMinToDecDeg (float degMin) {
 void Knock_Out()
 {
   // Set pin 2 as interrupt and attach handler:
-   attachInterrupt(0, wake, LOW);
-    delay(100);
-    
-    // Choose our preferred sleep mode:
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
- 
-    // Set sleep enable (SE) bit:
-    sleep_enable();
- 
-    // Put the device to sleep:
-    sleep_mode();
- 
-    // Upon waking up, sketch continues from this point.
-    sleep_disable();
+  attachInterrupt(0, wake, LOW);
+  delay(100);
+
+  // Choose our preferred sleep mode:
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+  // Set sleep enable (SE) bit:
+  sleep_enable();
+
+  // Put the device to sleep:
+  sleep_mode();
+
+  // Upon waking up, sketch continues from this point.
+  sleep_disable();
 }
 
 void wake ()
@@ -142,4 +152,5 @@ void wake ()
   // must do this as the pin will probably stay low for a while
   detachInterrupt (0);
 }  // end of wake
+
 
