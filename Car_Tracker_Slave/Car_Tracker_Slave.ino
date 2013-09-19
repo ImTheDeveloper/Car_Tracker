784b360bf5977d3923e869167e4b4327bfc3a335
christoface <chrismobberley@gmail.com>
2013-09-19 10:42:15 +0100

    Addition of interrupt code
    Interrupt code addition. Needs to be tweaked and tested. Upon initial
    setup the slave will sleep. After this it will then wait for the
    interrupt on pin 7 as LOW. It will then wait for serial data. Once
    printed it will then go back to sleep.

--- Car_Tracker_Slave/Car_Tracker_Slave.ino
+++ Car_Tracker_Slave/Car_Tracker_Slave.ino
@@ -13,23 +13,26 @@
 
 #include <SoftEasyTransfer.h>
 #include <SoftwareSerial.h>
+#include <Sleep_n0m1.h>
 
 //Construction
-SoftwareSerial mySerial2ndDuno(2, 3); /// main d3 to second d3 and main d2 to second d3
+SoftwareSerial mySerial2ndDuno(2, 3); /// main d3 to second d3 and main d2 to second d3 for communication to master
 SoftEasyTransfer ET; 
+Sleep sleep;
 
 //Definitions
 struct RECEIVE_DATA_STRUCTURE{
-  //put your variable definitions here for the data you want to receive
-  //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
   float latitude;
   float longitude;
+  char lon_string;
+  char lat_string;
 };
 
 //Declarations
 RECEIVE_DATA_STRUCTURE mydata;
 char latty[20];
 char longy[20];
+
 
 // ***************************************
 // Setup
@@ -38,8 +41,11 @@ char longy[20];
 void setup(){
   Serial.begin(38400);
   mySerial2ndDuno.begin(9600);
-  Serial.println("Starting up..");
   ET.begin(details(mydata), &mySerial2ndDuno); 
+  Serial.println("Initialisation complete.");
+  Serial.println("Going to sleep");
+  delay(100);
+  Knock_Out();
 }
 
 // ***************************************
@@ -50,34 +56,28 @@ void setup(){
 void loop(){
   //check and see if a data packet has come in. 
   if(ET.receiveData()){
-    //this is how you access the variables. [name of the group].[variable name]
-    //since we have data, we will blink it out. 
-  
      Serial.println(mydata.latitude);
      Serial.println(mydata.longitude);
-   
+     
    //Convert the inbound floats/doubles/ints into string ready for sending to json
      String lat = dtostrf(convertDegMinToDecDeg(mydata.latitude),10,6,latty);
-      String lon = dtostrf(convertDegMinToDecDeg(mydata.longitude),10,6,longy);
-    
+     String lon = dtostrf(convertDegMinToDecDeg(mydata.longitude),10,6,longy);
     //trim and turn them in to correct negative or positives // Need to pass in GPS.lon and GPS.lat at later date
-        lat.trim();
+      lat.trim();
       lon.trim();
-      
-       if (GPS.lon == 'W'){
-        lon = "-" + lon;
-       }
-       
-       if (GPS.lat == 'S'){
-         lat = "-" + lat;
-       }
+//      
+//       if (mydata.lon_string == 'W'){
+//        lon = "-" + lon;
+//       }
+//       
+//       if (mydata.lat_string == 'S'){
+//         lat = "-" + lat;
+//       }
+    Serial.print("{\"location\": {\"disposition\": \"mobile\",\"name\": \"Car Location\",\"exposure\": \"outdoor\", \"domain\": \"physical\",\"ele\": \"0000\",\"lat\": "+lat+",\"lon\": "+lon+"}}");
     
-    
-    // Serial.print("{\"location\": {\"disposition\": \"mobile\",\"name\": \"Car Location\",\"exposure\": \"outdoor\", \"domain\": \"physical\",\"ele\": \"0000\",\"lat\": "+lat+",\"lon\": "+lon+"}}");
-     
-    
+    //Go to sleep
+    Knock_Out();
     }
-  
   //you should make this delay shorter then your transmit delay or else messages could be lost
   delay(250);
 }
@@ -110,17 +110,22 @@ String int2fw (int x, int n) {
   return s;
 }
 
-// degree-minute format to decimal-degrees
+  // degree-minute format to decimal-degrees
 double convertDegMinToDecDeg (float degMin) {
   double min = 0.0;
   double decDeg = 0.0;
- 
   //get the minutes, fmod() requires double
   min = fmod((double)degMin, 100.0);
- 
   //rebuild coordinates in decimal degrees
   degMin = (int) ( degMin / 100 );
   decDeg = degMin + ( min / 60 );
- 
   return decDeg;
 }
+
+void Knock_Out()
+{
+  sleep.pwrDownMode(); //set sleep mode
+  sleep.sleepInterrupt(0,LOW); //sleep for: sleepTime
+}
+
+
